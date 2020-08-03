8b06162be3ad4cfe28aed78dfe718f6c22f487cc
abdulazeez-dolapo <abdulrafiua10@gmail.com>
2020-08-03 08:45:43 +0100

    add component to display income and expense total

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react"
 import { Header } from "./components/Header"
 import { Balance } from "./components/Balance"
+import { IncomeExpense } from "./components/IncomeExpense"
 
 function App() {
 	return (
 		<div className="App">
 			<Header />
 			<Balance />
+			<IncomeExpense />
 		</div>
 	)
 }
---
+++ src/components/IncomeExpense.js
@@ -0,0 +1,19 @@
+import React from "react"
+
+export const IncomeExpense = () => {
+	return (
+		<div>
+			<div>
+				<span>INCOME</span>
+
+				<span>$340</span>
+			</div>
+
+			<div>
+				<span>EXPENSE</span>
+
+				<span>$300</span>
+			</div>
+		</div>
+	)
+}
